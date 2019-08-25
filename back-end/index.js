1dd2473a9561a259452edb0e5def47f21910add6
ChrisSHDev <kakeochee@naver.com>
2019-08-24 19:47:37 -0700

    fix the front folder disabled

---
+++ back-end/index.js
@@ -0,0 +1,22 @@
+const express =require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const users = require('./routes/users');
+
+
+// setup environment
+dotenv.config();
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.use('/api/users', users);
+
+// mongo db connect
+mongoose.connect(process.env.MONGODB_URL, {userNewUrlParser: true});
+const PORT = process.env.PORT || 5000;
+
+// run app
+app.listen(process.env.PORT, () => console.log(`servser is running on port ${PORT}`));
---
+++ back-end/models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    login: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String
+    }
+
+});
+
+module.export = mongoose.model('User', userSchema);
