dbadc10c44f90012d23c0d49d3de6c831605672d
ChrisSHDev <kakeochee@naver.com>
2019-09-02 21:20:44 -0700

    implemented log-in header and fixed token auth error

--- front-end/src/actions/authActions.js
+++ front-end/src/actions/authActions.js
@@ -1,3 +1,4 @@
+
 import axios from 'axios';
 import { GET_ERRORS, SET_CURRENT_USER } from '../constants';
 import setAuthHeader from '../utils/setAuthHeader';
--- front-end/src/components/Layout/Header.js
+++ front-end/src/components/Layout/Header.js
@@ -1,28 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import { Link } from 'react-router-dom';
 import { withStyles } from '@material-ui/core/styles';
-
+import { connect } from 'react-redux';
+import { Icon } from '@material-ui/core';
 
 const styles = {
     root: {
         flexGrow: 1
+    },
+    logo: {
+        color: '#fff',
+        fontSize: 30
     }
 }
 
-const Header = ( { classes }) => {
-    return (
-        <div className={classes.root}>
-            <AppBar position="static">
-                <Toolbar>
-                    <Typography variant="h6" color="inherit">
-                        ReactBird - V1
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-        </div>
-    )
+class Header extends Component {
+    state = {
+        anchorEl : null
+    }
+
+    handleMenu = (event) => { this.ListeningStateChangedEvent({ anchorEl: event.target.currentTarget})};
+
+    handleColor = () => { this.setState({ anchorEl: null })}
+    
+    render() {
+        const { classes } = this.props;
+        const { anchorEl } = this.state;
+        const open = Boolean(anchorEl);
+
+        const authLinks = (
+            <div>
+                <IconButton
+                    aria-owns={ open ? 'menu' : undefined }
+                    aria-haspopup= "true"
+                    color = "inherit"
+                    onClick = {this.handleMenu}
+                >
+                    <AccountCircleIcon/>
+                </IconButton>
+                <Menu
+                    open= {open}
+                    anchorEl= { anchorEl }
+                >
+                    <MenuItem onClick={ this.handleclose}>Profile</MenuItem>
+                    <MenuItem onClick={ this.handleclose}>Logout</MenuItem>
+                </Menu>
+            </div>
+        )
+        return (
+            <div className={classes.root}>
+                <AppBar position="static" style={{ backgroundColor: '#4b0082' }}>
+                    <Toolbar>
+                        <Link to="/" className={classes.logo}>ReactBird</Link>
+                        { authLinks }
+                    </Toolbar>
+                </AppBar>
+            </div>
+        )
+    }
 }
 
-export default withStyles(styles)(Header)
+const mapStateToProps = (state) => ({
+    isAuthenticated: state.auth.isAuthenticated
+})
+
+export default connect(mapStateToProps)(withStyles(styles)(Header));
